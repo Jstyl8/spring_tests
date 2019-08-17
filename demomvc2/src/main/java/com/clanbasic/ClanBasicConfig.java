ccff03f7d6bcad0dbf4f2696a1f0df8faf76abd0
Jstyl8 <joseangel.jstyl8@gmail.com>
2019-08-17 10:11:38 +0200

    Use ViewResolver to avoid jsp harcoding. Move jsp to web-inf

--- demomvc2/src/main/java/com/clanbasic/ClanBasicConfig.java
+++ demomvc2/src/main/java/com/clanbasic/ClanBasicConfig.java
@@ -1,10 +1,19 @@
 package com.clanbasic;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan({"com.clanbasic"})
 public class ClanBasicConfig {
 
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver vr = new InternalResourceViewResolver();
+        vr.setPrefix("/WEB-INF/");
+        vr.setSuffix(".jsp");
+        return vr;
+    }
 }
